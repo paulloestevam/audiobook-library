0756584c346b3ad0a0edf13fdff0e3e2cac6d90d
Paullo Estevam <paulloestevam@gmail.com>
2026-04-20 14:08:27 -0300

    Initial commit: Audiobook Library project

---
+++ src/main/java/com/paulloestevam/audiobook_library/AudiobookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.paulloestevam.audiobook_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiobookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiobookLibraryApplication.class, args);
+	}
+
+}
